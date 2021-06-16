f5acdf5b72730ed1013e234aa506c69b09fd90ed
Aman <amangupta71010@gmail.com>
2021-06-17 01:19:30 +0530

    level 3- add environment vars

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 //jshint esversion:6
 
+require('dotenv').config();
 const express = require('express');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
@@ -24,8 +25,7 @@ const userSchema = new mongoose.Schema({
 })
 
 
-const secret = "thisismysecret";
-userSchema.plugin(encrypt, {secret: secret, encryptedFields: ['password']});
+userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});
 
 const User = mongoose.model("User", userSchema);
 
