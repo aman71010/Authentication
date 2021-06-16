1de546ca210165f865aaef6e28c8f8b702c068ee
Aman <amangupta71010@gmail.com>
2021-06-17 00:55:01 +0530

    level 2- data encryption

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption');
 
 const app = express();
 
@@ -21,6 +22,10 @@ const userSchema = new mongoose.Schema({
     email: String,
     password: String
 })
+
+
+const secret = "thisismysecret";
+userSchema.plugin(encrypt, {secret: secret, encryptedFields: ['password']});
 
 const User = mongoose.model("User", userSchema);
 
